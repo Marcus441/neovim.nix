b269fb15158b2af3f437361f9876648647a79baf
Marcus441 <psminchia4153@hotmail.com>
2026-04-03 11:11:51 +1000

    feat(dashboard): add option for search projects

--- dashboard.nix
+++ dashboard.nix
@@ -44,39 +44,34 @@ in {
       theme = null;
 
       layout = [
-        # --- 1. TOP PADDING ---
         {
           type = "padding";
           val = 8;
         }
 
-        # --- 2. HEADER (Applying the custom color) ---
         {
           type = "text";
           val = header;
           opts = {
             position = "center";
-            # Use the custom highlight group defined above
             hl = "DashboardHeader";
           };
         }
 
-        # --- 3. SEPARATOR PADDING ---
         {
           type = "padding";
           val = 2;
         }
 
-        # --- 4. BUTTONS ---
         {
           type = "group";
           val = [
             (mkButton "e" "" "> New file" "ene | startinsert")
             (mkButton "f" "" "> Find file" "Telescope find_files")
-            (mkButton "g" "" "> Live Grep" "Telescope live_grep")
-            (mkButton "r" "" "> Recent" "Telescope oldfiles")
+            (mkButton "g" "󰈞" "> Live Grep" "Telescope live_grep")
+            (mkButton "r" "󰙰" "> Recent" "Telescope oldfiles")
+            (mkButton "p" "" "> Search Projects" "Telescope projects")
             (mkButton "l" "" "> Last session" "SessionManager load_last_session")
-            (mkButton "s" "" "> Settings" "e $HOME/flake | :cd %:p:h | split . | wincmd k | pwd")
             (mkButton "q" "" "> Quit NVIM" "qa")
           ];
           opts = {
