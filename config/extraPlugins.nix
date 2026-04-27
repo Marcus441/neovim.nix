e72bc22e80c3a58dbc821d97eb6ce0e3fb5d01a9
Marcus441 <psminchia4153@hotmail.com>
2026-04-27 12:09:53 +1000

    refactor: remove vim-tmux-navigator from nvim

--- config/extraPlugins.nix
+++ config/extraPlugins.nix
@@ -4,17 +4,9 @@
       package = pkgs.vimPlugins.kanagawa-nvim;
       setup = builtins.readFile ./lua/kanagawa-setup.lua;
     };
-    vim-tmux-navigator = {
-      package = pkgs.vimPlugins.vim-tmux-navigator;
-      setup = "
-          vim.g.tmux_navigator_no_mappings = 1
-          vim.g.tmux_navigator_no_wrap = 1
-          ";
-    };
     undotree = {
       package = pkgs.vimPlugins.undotree;
-      setup = "";
-      after = ["vim-tmux-navigator"];
+      after = ["theme-plugin"];
     };
   };
 }
